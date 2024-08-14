358579118da509959508c320f955a57c4d177ead
Sheegan Sri G M <sheegangm@gmail.com>
2024-08-14 09:16:53 +0530

    Implementation of ml model evaluation techniques

---
+++ ML-ModelEvaluationTechnique-2/F1-Score.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import f1_score 
+
+y_true = [1, 0, 1, 1, 0, 1, 0, 0, 1, 0] 
+y_pred = [1, 0, 1, 1, 0, 0, 1, 0, 1, 1] 
+
+f1 = f1_score(y_true, y_pred) 
+
+print("F1 Score:", f1)
